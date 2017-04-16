3897ceaa644d60397365570fd597e834d95cd077
ThiagoMendes <ThiagoMendes@ThiagoMendes-PC>
2017-04-16 11:40:37 -0300

    Commit 001 - Montagem dos Projetos Common, Persitence e Service

---
+++ dogkit-common/src/main/java/br/com/dogkit/common/bean/UsuarioBean.java
@@ -0,0 +1,72 @@
+package br.com.dogkit.common.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class UsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 8770767756202274004L;
+	private Long id;
+	private String nomeCompleto;
+	private String email;
+	private String telefone;
+	private String sexo;
+	private Date dataCriacao;
+	private Date dataModificacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataModificacao() {
+		return dataModificacao;
+	}
+
+	public void setDataModificacao(Date dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+}
---
+++ dogkit-persistence/src/main/java/br/com/dogkit/persistence/dao/IUsuarioDao.java
@@ -0,0 +1,19 @@
+package br.com.dogkit.persistence.dao;
+
+import java.util.List;
+
+import br.com.dogkit.common.bean.UsuarioBean;
+
+public interface IUsuarioDao {
+
+	public List<UsuarioBean> getUsuarios();
+
+	public UsuarioBean getUsuario(Integer id);
+
+	public void putUsuario(UsuarioBean usuarioBean);
+
+	public void deleteUsuario(UsuarioBean usuarioBean);
+
+	public void postUsuario(UsuarioBean usuarioBean);
+
+}
